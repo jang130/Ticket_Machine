2769ccb88911be0268812628b67519ea898c7503
Adam <adm.moszczynski@gmail.com>
2021-12-23 22:02:22 +0100

    added time module

--- system.py
+++ system.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date, time
+
 class WrongOptionError(Exception):
     def __init__(self):
         super().__init__('Wrong choice')
@@ -24,3 +26,11 @@ def problem_report():
 def choice():
     choice_input = str(input())
     return choice_input
+
+def time_module():
+    currenttime = datetime.now()
+    currentdate = date.today()
+    formatted_date = currentdate.strftime("%d/%m/%Y")
+    formatted_time = currenttime.strftime("%H:%M:%S")
+    return (formatted_time, formatted_date)
+
