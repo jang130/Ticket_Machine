f7b29102878cdf608c171b6476d6581d696dcf03
Adam <adm.moszczynski@gmail.com>
2022-01-03 21:02:16 +0100

    repaired ticket class

--- ticket_class.py
+++ ticket_class.py
@@ -1,9 +1,6 @@
 class ticket:
-    def __init__(self,ticket_id, ticket_type, ticket_date):
+    def __init__(self, info_list):
+        ticket_id, ticket_type, ticket_date = info_list
         self.ticket_id = ticket_id
         self.ticket_type = ticket_type
         self.ticket_date = ticket_date
-
-
-
-
