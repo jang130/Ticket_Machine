b89a5b03255daee56b80b8b20a59c5ebd89cc8a1
Adam <adm.moszczynski@gmail.com>
2022-01-04 12:58:55 +0100

    started buy ticket tests

--- test_system.py
+++ test_system.py
@@ -1,16 +1,32 @@
-from system import machine_system
+from system import TicketAlreadyExistsError, TimeTicketAlreadyExistsError, machine_system
+import pytest
 
 def test_system():
     machine = machine_system()
 
-def test_machine_system_class_any_ticket():
+def test_machine_system_class_buy_any_ticket():
     operation = machine_system()
+    operation.files_reset()
     operation.load_file('Customer_data')
-    operation.tickets_load('Ticket_data') #dodać testowe bazy
+    operation.tickets_load('Ticket_data')
     short_term_ticket =(('Doralyn','Dovermann'),'20min')
+    short_term_ticket2 =(('Doralyn','Dovermann'),'24h')
     time_ticket = (('Doralyn','Dovermann'),'1y')
     assert operation.system_ticket(short_term_ticket) == 'Succeed'
+    assert operation.system_ticket(short_term_ticket2) == 'Succeed'
     assert operation.system_ticket(time_ticket) == 'Succeed'
+
+def test_machine_system_class_buy_two_time_tickets():
+    operation = machine_system()
+    operation.files_reset()
+    operation.load_file('Customer_data')
+    operation.tickets_load('Ticket_data')
+    short_term_ticket =(('Doralyn','Dovermann'),'20min')
+    time_ticket = (('Doralyn','Dovermann'),'1y')
+    with pytest.raises(TimeTicketAlreadyExistsError):
+        assert operation.system_ticket(time_ticket) == 'Succeed'
+        assert operation.system_ticket(time_ticket) == 'Succeed'
+
 
 
 
@@ -18,3 +34,5 @@ def test_machine_system_class_any_ticket():
     #monkeypatch.setattr('system.machine_system.choice', choice())
     #monkeypatch.setattr('system.machine_system', operation.choice())
     #assert operation.choice() == '2'
+#operation.write_file('Customer_data')
+#operation.tickets_write('Ticket_data')
