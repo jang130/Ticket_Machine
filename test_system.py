a4030a33a5aa7aabf9729b475a368d65400fa750
Adam <adm.moszczynski@gmail.com>
2022-01-07 13:48:43 +0100

    written full test

--- test_system.py
+++ test_system.py
@@ -1,30 +1,35 @@
-from os import name
-from system import NotEnoughMoneyError, PersonNotFoundError, TicketDoesNotExistError, TimeTicketAlreadyExistsError, machine_system
+
+from system import NotEnoughMoneyError, PersonNotFoundError, machine_system
+from system import TicketDoesNotExistError, TimeTicketAlreadyExistsError
+from system import MissingFileError
 import pytest
 import io
 from datetime import datetime, date
 
+
 def test_system():
     machine_system()
+
 
 def test_machine_system_class_buy_any_ticket():
     operation = machine_system()
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    short_term_ticket =(('Doralyn','Dovermann'),'20min')
-    short_term_ticket2 =(('Doralyn','Dovermann'),'24h')
-    time_ticket = (('Doralyn','Dovermann'),'1y')
+    short_term_ticket = (('Doralyn', 'Dovermann'), '20min')
+    short_term_ticket2 = (('Doralyn', 'Dovermann'), '24h')
+    time_ticket = (('Doralyn', 'Dovermann'), '1y')
     assert operation.system_ticket(short_term_ticket) == 'Succeed'
     assert operation.system_ticket(short_term_ticket2) == 'Succeed'
     assert operation.system_ticket(time_ticket) == 'Succeed'
+
 
 def test_machine_system_class_buy_two_time_tickets():
     operation = machine_system()
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    time_ticket = (('Doralyn','Dovermann'),'1y')
+    time_ticket = (('Doralyn', 'Dovermann'), '1y')
     with pytest.raises(TimeTicketAlreadyExistsError):
         assert operation.system_ticket(time_ticket) == 'Succeed'
         assert operation.system_ticket(time_ticket) == 'Succeed'
@@ -35,22 +40,23 @@ def test_machine_system_class_check_time_ticket_and_calc_expiry():
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    time_ticket = (('Doralyn','Dovermann'),'1y')
+    time_ticket = (('Doralyn', 'Dovermann'), '1y')
     customer = operation.customers[0]
     assert operation.system_ticket(time_ticket) == 'Succeed'
-    name = (('Doralyn','Dovermann'))
+    name = (('Doralyn', 'Dovermann'))
     check_ticket = operation.system_check_ticket(name)
     expiry = operation.calculate_expiry_date(customer)
     assert check_ticket == expiry
+
 
 def test_ticket_split():
     operation = machine_system()
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    ticket1 = (('Doralyn','Dovermann'),'20min')
-    ticket2 = (('Doralyn','Dovermann'),'75min')
-    ticket3 = (('Doralyn','Dovermann'),'24h')
+    ticket1 = (('Doralyn', 'Dovermann'), '20min')
+    ticket2 = (('Doralyn', 'Dovermann'), '75min')
+    ticket3 = (('Doralyn', 'Dovermann'), '24h')
     operation.system_ticket(ticket1)
     operation.system_ticket(ticket2)
     operation.system_ticket(ticket3)
@@ -86,10 +92,10 @@ def test_charge_money_insufficient_funds():
 
 def test_buy_ticket_method():
     operation = machine_system()
-    buy_ticket1 = operation.buy_ticket('15000','20min')
-    buy_ticket2 = operation.buy_ticket('15000','75min')
-    buy_ticket3 = operation.buy_ticket('15000','24h')
-    buy_ticket4 = operation.buy_ticket('15000','72h')
+    buy_ticket1 = operation.buy_ticket('15000', '20min')
+    buy_ticket2 = operation.buy_ticket('15000', '75min')
+    buy_ticket3 = operation.buy_ticket('15000', '24h')
+    buy_ticket4 = operation.buy_ticket('15000', '72h')
     assert buy_ticket1 == '14660'
     assert buy_ticket2 == '14560'
     assert buy_ticket3 == '13500'
@@ -101,10 +107,11 @@ def test_buy_ticket_method_insufficient_funds():
     with pytest.raises(NotEnoughMoneyError):
         operation.buy_ticket('1500', '72h')
 
+
 def test_date_split_method():
     operation = machine_system()
     date_split = operation.date_split('12:04:35 28/12/2021')
-    assert date_split == (12,4,35,28,12,2021)
+    assert date_split == (12, 4, 35, 28, 12, 2021)
 
 
 def test_unique_id_for_ticket_method():
@@ -120,8 +127,9 @@ def test_system_prepaid_check_method():
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    prepaid = operation.system_prepaid_check(('Doralyn','Dovermann'))
+    prepaid = operation.system_prepaid_check(('Doralyn', 'Dovermann'))
     assert prepaid == ['26']
+
 
 def test_system_prepaid_check_method_missing_person():
     operation = machine_system()
@@ -129,8 +137,7 @@ def test_system_prepaid_check_method_missing_person():
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
     with pytest.raises(PersonNotFoundError):
-        operation.system_prepaid_check(('Doralyn','Doralyn'))
-
+        operation.system_prepaid_check(('Doralyn', 'Doralyn'))
 
 
 def test_system_prepaid_check_method_missing_ticket():
@@ -139,7 +146,7 @@ def test_system_prepaid_check_method_missing_ticket():
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
     with pytest.raises(TicketDoesNotExistError):
-        operation.system_prepaid_check(('Whitney','Girardoni'))
+        operation.system_prepaid_check(('Whitney', 'Girardoni'))
 
 
 def test_system_funds_check_method():
@@ -147,7 +154,7 @@ def test_system_funds_check_method():
     operation.files_reset()
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
-    funds = operation.system_funds_check(('Whitney','Girardoni'))
+    funds = operation.system_funds_check(('Whitney', 'Girardoni'))
     assert funds == (1000, 0)
 
 
@@ -157,7 +164,8 @@ def test_system_funds_check_method_missing_person():
     operation.load_file('Customer_data')
     operation.tickets_load('Ticket_data')
     with pytest.raises(PersonNotFoundError):
-        operation.system_funds_check(('Whitney','Whitney'))
+        operation.system_funds_check(('Whitney', 'Whitney'))
+
 
 def test_choice(monkeypatch):
     operation = machine_system()
@@ -179,4 +187,53 @@ def test_time_module():
     assert operation.time_module(True) == (time, today)
 
 
-def test_load_file()
+def test_load_file():
+    operation = machine_system()
+    operation.files_reset()
+    operation.load_file('Customer_data')
+
+
+def test_load_wrong_file():
+    operation = machine_system()
+    operation.files_reset()
+    with pytest.raises(MissingFileError):
+        operation.load_file('Customer_datatata')
+
+
+def test_write_file():
+    operation = machine_system()
+    operation.files_reset()
+    operation.load_file('Customer_data')
+    operation.write_file('Customer_data')
+
+
+def test_tickets_load():
+    operation = machine_system()
+    operation.tickets_load('Ticket_data')
+
+
+def test_tickets_load_wrong_file():
+    operation = machine_system()
+    with pytest.raises(MissingFileError):
+        operation.tickets_load('Ticket_datatata')
+
+
+def test_tickets_write():
+    operation = machine_system()
+    operation.tickets_load('Ticket_data')
+    operation.tickets_write('Ticket_data')
+
+
+def test_error_log_check():
+    operation = machine_system()
+    operation.error_log(MissingFileError)
+
+
+def test_error_log_check_problem_report():
+    operation = machine_system()
+    operation.error_log('Sample error description')
+
+
+def test_reset_files():
+    operation = machine_system()
+    operation.files_reset()
