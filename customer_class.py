75af6f9aed5c44125684a8e9385647796a556d51
Adam <adm.moszczynski@gmail.com>
2022-01-07 13:48:03 +0100

    added docstrings

--- customer_class.py
+++ customer_class.py
@@ -1,9 +1,16 @@
-class IncorrectDataError(Exception):
-    def __init__(self) -> None:
-        super().__init__('This customer info is not correct')
-
 class customer:
+    '''
+    Class that represents customer. Read from
+    Customer_data.txt
+    '''
     def __init__(self, info_list):
+        '''
+        Initial method
+
+        :param info_list:
+        :type tuple:
+        :format (id, fname, lname, ticket_id, funds)
+        '''
         id, fname, lname, ticket_id, funds = info_list
         self.id = id
         self.fname = fname
--- ticket_class.py
+++ ticket_class.py
@@ -1,5 +1,16 @@
 class ticket:
+    '''
+    Class that represents ticket. Read from
+    Ticket_data.txt
+    '''
     def __init__(self, info_list):
+        '''
+        Initial method
+
+        :param info_list:
+        :type tuple:
+        :format (ticket_id, ticket_type, ticket_date)
+        '''
         ticket_id, ticket_type, ticket_date = info_list
         self.ticket_id = ticket_id
         self.ticket_type = ticket_type
