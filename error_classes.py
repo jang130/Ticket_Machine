02c73c97dc92ac92a08586d1d092f842c9add5f2
Adam <adm.moszczynski@gmail.com>
2022-01-11 13:29:42 +0100

    changed errors to error classes

---
+++ error_classes.py
@@ -0,0 +1,37 @@
+class WrongOptionError(Exception):
+    def __init__(self):
+        super().__init__('Wrong choice')
+
+class WrongAmountOfAtributesInClassError(Exception):
+    def __init__(self):
+        super().__init__('Cannot generate object too many or to less atributes')
+
+
+class PersonNotFoundError(Exception):
+    def __init__(self):
+        super().__init__('Customer not found in database')
+
+
+class TicketAlreadyExistsError(Exception):
+    def __init__(self):
+        super().__init__('Customer already have ticket')
+
+
+class TicketDoesNotExistError(Exception):
+    def __init__(self):
+        super().__init__('Customer does not have ticket')
+
+
+class TimeTicketAlreadyExistsError(Exception):
+    def __init__(self):
+        super().__init__('There can only be one time ticket')
+
+
+class NotEnoughMoneyError(Exception):
+    def __init__(self):
+        super().__init__('Insufficient funds')
+
+
+class MissingFileError(FileNotFoundError):
+    def __init__(self):
+        super().__init__('Data file not found')
